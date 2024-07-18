5edb1b3f2348399a83ea4ef5ba168d1c4056f04f
SlyPex <59923212+SlyPex@users.noreply.github.com>
2024-07-18 15:36:58 +0100

    updated workflow v9

---
+++ .github/scripts/dockerfile_checker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if  [[ "$#" -gt 1 ]]
+then
+	echo -e "[!] - ERROR : Extra Argument Specified\nUsage : $0 [path_to_directory]"
+	exit 1
+elif [[ "$#" -ne 1 ]]
+then
+	echo -e "[!] - ERROR : Missing Argument\nUsage : $0 [path_to_directory]"
+	exit 1
+else
+	if [[ ! -d "$1" ]] then
+		echo "[!] - ERROR : The specified Argument $1 is not a directory"
+		exit 1
+	else
+		ls -lA $1 | grep -qo Dockerfile
+	fi
+fi
